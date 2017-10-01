13e4016f6b73a268de287b7b848df7d569598784
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-02 00:46:04 +0200

    New commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var fs = require("fs");
 
-fs.readdir("../", "utf-8", function(err, files){
+fs.readdir("../", "utf-8", function(err, data){
     if (err) throw err;
-    fs.writeFile("./tekst.txt", files, "utf-8", function(err) {
+    fs.writeFile("./tekst.txt", data.join("\n"), "utf-8", function(err) {
         if (err) throw err;
-        console.log(files);
+        console.log(data);
     })
 });
 
